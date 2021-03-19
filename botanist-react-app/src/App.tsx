43af0e9f481cf2eb83153dd49a230b349bbeb12c
Blito <pabloarubi@gmail.com>
2021-03-18 21:18:57 -0700

    Create basic structure for the App
    - The App was created with `npx create-react-app botanist-react-app --template typescript`.
    - Dockerfile set up to run a development server with hot reloading.
    - Added `docker-compose.yml` file and scripts ready for easy start/shutdown access.
    - Also added a CONTRIBUTE.md file with basic info on how to run things.
    
    I put the actual react App behind the `botanist-react-app` directory
    since we may want to add more parts of the system in this same repo
    down the road. Same reason why I went for a docker-compose file as
    opposed to just a `Dockerfile`.

---
+++ botanist-react-app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello World!
+        <div className="Card">
+          <div className="CardText">botanist</div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
